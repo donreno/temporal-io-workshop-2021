62c04c4f011e10c6df9831a0150e6377d271e529
Renato <renatosmb@gmail.com>
2021-06-21 18:14:11 -0400

    Refactor de verificacion de cliente

--- workflow/workflow.go
+++ workflow/workflow.go
@@ -3,6 +3,7 @@ package workflow
 import (
 	"time"
 
+	"go.temporal.io/sdk/log"
 	"go.temporal.io/sdk/workflow"
 )
 
@@ -21,13 +22,23 @@ func TransferWorkflow(ctx workflow.Context, transfer Transfer) error {
 	ctx = workflow.WithActivityOptions(ctx, activityOptions)
 	logger := workflow.GetLogger(ctx)
 
+	if err := verifyCustomer(ctx, logger, transfer); err != nil {
+		notifyFailedTransfer(ctx, transfer)
+		return err
+	}
+
+	notifySuccessfulTransfer(ctx, transfer)
+
+	return nil
+}
+
+func verifyCustomer(ctx workflow.Context, logger log.Logger, transfer Transfer) error {
 	getCustomerInfoExec := workflow.ExecuteActivity(ctx, GetCustomerDetails, transfer.Origin)
 	isCustomerRiskyExec := workflow.ExecuteActivity(ctx, IsRiskyCustomer, transfer.Origin)
 
 	var customerName string
 	err := getCustomerInfoExec.Get(ctx, &customerName)
 	if err != nil {
-		notifyFailedTransfer(ctx, transfer)
 		logger.Error("Error obteniendo informacion de cliente", transfer.Origin)
 		return err
 	}
@@ -35,14 +46,13 @@ func TransferWorkflow(ctx workflow.Context, transfer Transfer) error {
 	var isRisky bool
 	err = isCustomerRiskyExec.Get(ctx, &isRisky)
 	if err != nil {
-		notifyFailedTransfer(ctx, transfer)
 		logger.Error("Error Resolviendo riesgo de cliente", transfer.Origin)
 		return err
 	}
 
 	if isRisky {
-		notifyFailedTransfer(ctx, transfer)
 		logger.Error("Cliente", transfer.Origin, "es riesgoso")
+		return err
 	}
 
 	logger.Info("Cliente ", customerName, "Numero de cuenta", transfer.Origin, "no es riesgoso")
@@ -52,4 +62,8 @@ func TransferWorkflow(ctx workflow.Context, transfer Transfer) error {
 
 func notifyFailedTransfer(ctx workflow.Context, transfer Transfer) {
 	workflow.ExecuteActivity(ctx, NotifyFailedTransfer, transfer.Origin, transfer.Destination, transfer.Amount)
+}
+
+func notifySuccessfulTransfer(ctx workflow.Context, transfer Transfer) {
+	workflow.ExecuteActivity(ctx, NotifySuccessfulTransfer, transfer.Origin, transfer.Destination, transfer.Amount)
 }
