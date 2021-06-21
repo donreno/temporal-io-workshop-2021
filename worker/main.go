2341b53cda63ca0ccecbe5ae77d1466995ef16d5
Renato <renatosmb@gmail.com>
2021-06-21 19:39:28 -0400

    Agrega worker

--- worker/main.go
+++ worker/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/donreno/temporal-io-workshop-2021/workflow"
+	"go.temporal.io/sdk/client"
+	"go.temporal.io/sdk/worker"
+)
 
 func main() {
-	log.Println("Worker incializado...")
+	c, err := client.NewClient(client.Options{})
+	if err != nil {
+		log.Fatalln("Error al crear cliente", err)
+	}
+
+	defer c.Close()
+
+	w := worker.New(c, "transfer-workflow", worker.Options{})
+
+	w.RegisterWorkflow(workflow.TransferWorkflow)
+	w.RegisterActivity(workflow.GetCustomerDetails)
+	w.RegisterActivity(workflow.IsRiskyCustomer)
+	w.RegisterActivity(workflow.ChargeAccount)
+	w.RegisterActivity(workflow.PayToAccount)
+	w.RegisterActivity(workflow.RevertCharge)
+	w.RegisterActivity(workflow.RevertPayment)
+	w.RegisterActivity(workflow.NotifyFailedTransfer)
+	w.RegisterActivity(workflow.NotifySuccessfulTransfer)
+
+	if err = w.Run(worker.InterruptCh()); err != nil {
+		log.Fatalln("Error ejecutando worker", err)
+	}
 }
