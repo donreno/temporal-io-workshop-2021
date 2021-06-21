8e0019018c11512329a7eea9368ef15c8955fd0d
Renato <renatosmb@gmail.com>
2021-06-21 19:33:00 -0400

    Agrega starter

--- starter/main.go
+++ starter/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/donreno/temporal-io-workshop-2021/workflow"
+	"github.com/gofiber/fiber/v2"
+	"go.temporal.io/sdk/client"
+)
 
 func main() {
-	log.Println("Starter incializado...")
+	c, err := client.NewClient(client.Options{})
+	if err != nil {
+		log.Fatalln("Error al crear cliente", err)
+	}
+
+	defer c.Close()
+
+	app := fiber.New()
+
+	workflowOpts := client.StartWorkflowOptions{
+		ID:        "transfer-workflow",
+		TaskQueue: "transfer-workflow-queue",
+	}
+
+	app.Post("/transfer", func(ctx *fiber.Ctx) error {
+		var transfer workflow.Transfer
+		ctx.BodyParser(&transfer)
+
+		exec, err := c.ExecuteWorkflow(ctx.Context(), workflowOpts, workflow.TransferWorkflow, transfer)
+		if err != nil {
+			log.Println("Error iniciando workflow", err)
+			return ctx.Status(500).SendString("Error iniciando workflow")
+		}
+
+		log.Println("Workflow ID", exec.GetID(), "| Run ID", exec.GetRunID())
+
+		if err = exec.Get(ctx.Context(), nil); err != nil {
+			log.Println("Error obteniendo resultado de workflow", err)
+			return ctx.Status(500).SendString("Error obteniendo resultado de workflow")
+		}
+
+		return ctx.Status(200).SendString("Transferencia realizada de forma exitosa!")
+	})
+
+	log.Fatal(app.Listen(":3000"))
 }
